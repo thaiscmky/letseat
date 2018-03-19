c652208840eb5162127c3921eb30559d60ac85f7
Thais Cailet <thais.cailet@gmail.com>
2018-03-19 13:37:28 -0500

    #36 Moves db JSON into a callable JavaScript function

--- assets/javascript/tests.js
+++ assets/javascript/tests.js
@@ -16,3 +16,22 @@ function testApiCall(options, apitoken){
         console.log(secureApiRequest.responseObject);
     });
 }
+
+function getDbStructure() {
+    return {
+        "event" : {
+            "date" : "today(march 18)",
+            "eventKey" : "2nub33ne7b4b4734bbeus(unique id) ",
+            "eventName" : "eating pizza ",
+            "location" : "downtown",
+            "time" : "10:00" //optional
+        },
+        "user" : {
+            "email" : "tom@gmail.com",
+            "eventKey" : "2nub33ne7b4b4734bbeus", //It's same with eventkey; this will enable us to easily get the group which a user belongs to.
+            "name " : "Tom",
+            "time" : "10:00",
+            "zipcode" : 77002
+        }
+    };
+}
