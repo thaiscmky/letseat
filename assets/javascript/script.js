126bfc63f02d91b1683533c16ca0871ec962d959
Thais Cailet <thais.cailet@gmail.com>
2018-03-19 02:38:07 -0500

    #31 Demonstrates loading suggested food list from a KO component

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,25 @@ function MyFirstModelView() {
     this.lastName = "Cailet";
 }
 
+function Suggestion(name) {
+    this.name = name;
+}
+
+function SuggestionsViewModel() {
+    this.suggestions = [
+        new Suggestion('Fried Chicken'),
+        new Suggestion('Burger'),
+        new Suggestion('Pizza'),
+        new Suggestion('Pasta'),
+        new Suggestion('Hamburgers'),
+        new Suggestion('Food 1'),
+        new Suggestion('Food 2'),
+        new Suggestion('Food 3'),
+        new Suggestion('Food 4')
+    ];
+}
+
 $(document).ready(function(){
-    ko.applyBindings(new MyFirstModelView());
+    ko.applyBindings(new MyFirstModelView(), document.getElementById("dummy-data"));
+    ko.applyBindings(new SuggestionsViewModel(), document.getElementById("list"));
 });
