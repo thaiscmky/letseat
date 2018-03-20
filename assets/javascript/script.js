9286e2bfc5626647ff8eb2be8f44987b10264613
The Mai <Mai.The.09@gmail.com>
2018-03-20 12:15:51 -0500

    close #42 add dynamic Zipcode textbox

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+    function landingViewModel() {
+        var self = this;
+
+        self.zipCode = ko.observable('');
+
+        self.zipInfo = ko.observable('');
+
+        self.zipRequest = ko.computed(function () {
+            console.log(self.zipCode());
+            var info = '';
+            $.ajax({                
+                url: "http://maps.googleapis.com/maps/api/geocode/json?address=" + self.zipCode(),
+                method: "GET"
+            }).done(function (res) {                                
+                info = res.results[0].formatted_address;                                
+                self.zipInfo(info);
+            });                        
+        })
+    }
+
+    ko.applyBindings(new landingViewModel());
 })
