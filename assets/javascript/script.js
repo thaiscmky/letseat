7f8430f2a9486cf8d3b788bc7c6480c8f6f85680
The Mai <Mai.The.09@gmail.com>
2018-03-20 12:42:11 -0500

    #43 #42 #41 add KO submit components to search, add some comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,35 @@
-$(document).ready(function () {
+$(document).ready(function () {    
     function landingViewModel() {
         var self = this;
 
+        self.searchTerm = ko.observable('');
+        
         self.zipCode = ko.observable('');
 
         self.zipInfo = ko.observable('');
 
-        self.zipRequest = ko.computed(function () {
-            console.log(self.zipCode());
-            var info = '';
-            $.ajax({                
+        // Sends AJAX to google APIs and sets VM's zipInfo to relevant JSON data.
+        self.zipRequest = ko.computed(function () {                        
+            // TODO: VALIDATOR: Below ajax call should only run when "self.zipInfo() === valid Zip Code
+            // ELSE it should set self.zipInfo to something like "Zip Code not recognized"
+            $.ajax({
                 url: "http://maps.googleapis.com/maps/api/geocode/json?address=" + self.zipCode(),
                 method: "GET"
-            }).done(function (res) {                                
-                info = res.results[0].formatted_address;                                
+            }).done(function (res) {
+                var info = res.results[0].formatted_address;
                 self.zipInfo(info);
-            });                        
+            });
         })
+
+        self.submitSearch = function () {
+            var searchTerm = self.searchTerm();
+            var zipCode = self.zipCode();
+            // When user clicks submit this is the code that runs...
+            // FYI this prevent default submit functionality because we're using KO.js
+            console.log('TEST: Look in console');
+            console.log('Search: ' + searchTerm)
+            console.log('zipCode: ' + zipCode)
+        }
     }
 
     ko.applyBindings(new landingViewModel());
