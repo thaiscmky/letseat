79e6cf58563df4e8d72e746abc4017855fd8ed4f
Thais Cailet <thais.cailet@gmail.com>
2018-03-20 23:50:26 -0500

    Reverts non-working code inadvertedly pushed with commit for fixed cdns in script.js
    This reverts commit 3273c62f5f1a5766923351bf8b13d51b3bd878da.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,14 +3,14 @@ requirejs.config({
     "baseUrl": "./assets/javascript/lib",
     "paths": {
         "script": "../script",
-        "jquery": "https://code.jquery.com/jquery-3.3.1.min",
-        "bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min",
+        "jquery": "//code.jquery.com/jquery-3.3.1.min",
+        "bootstrap": "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min",
         "corsanywhere": "cors-anywhere",
-        "ko": 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min',
-        "koDebug": 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-debug',
+        "ko": '//cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min',
+        "koDebug": '//cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-debug',
         "koComponentRegister": 'ko-components-registration',
-        "text": 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text',
-        "domReady": 'https://cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady'
+        "text": '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text',
+        "domReady": '//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady'
 
     }
 });
