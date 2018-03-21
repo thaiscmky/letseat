8a32ad92a39f75098acfa9671329898a042e9a99
thaiscmky <22459337+thaiscmky@users.noreply.github.com>
2018-03-20 20:11:51 -0500

    Fixes library issues and specifies an element binding for the LandingViewModel

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,8 +4,7 @@ requirejs.config({
     "paths": {
         "script": "../script",
         "jquery": "//code.jquery.com/jquery-3.3.1.min",
-        "popper": "//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min",
-        "bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js",
+        "bootstrap": "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min",
         "corsanywhere": "cors-anywhere",
         "ko": '//cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min',
         "koDebug": '//cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-debug',
--- assets/javascript/script/main.js
+++ assets/javascript/script/main.js
@@ -1,4 +1,4 @@
-define(["jquery", "popper", "corsanywhere", "ko", "koDebug"], function($, cors, ko, kob) {
+define(["jquery", "bootstrap", "corsanywhere", "ko", "koDebug"], function($, bootstrap, cors, ko, kob) {
     function LandingViewModel() {
         var self = this;
 
@@ -12,13 +12,15 @@ define(["jquery", "popper", "corsanywhere", "ko", "koDebug"], function($, cors, 
         self.zipRequest = ko.computed(function () {
             // TODO: VALIDATOR: Below ajax call should only run when "self.zipInfo() === valid Zip Code
             // ELSE it should set self.zipInfo to something like "Zip Code not recognized"
-            $.ajax({
-                url: "http://maps.googleapis.com/maps/api/geocode/json?address=" + self.zipCode(),
-                method: "GET"
-            }).done(function (res) {
-                var info = res.results[0].formatted_address;
-                self.zipInfo(info);
-            });
+            if(self.zipCode() !== null || typeof self.zipCode() !== 'undefined'){
+                $.ajax({
+                    url: "http://maps.googleapis.com/maps/api/geocode/json?address=" + self.zipCode(),
+                    method: "GET"
+                }).done(function (res) {
+                    var info = res.results[0].formatted_address;
+                    self.zipInfo(info);
+                });
+            }
         });
 
         self.submitSearch = function () {
@@ -27,13 +29,14 @@ define(["jquery", "popper", "corsanywhere", "ko", "koDebug"], function($, cors, 
             // When user clicks submit this is the code that runs...
             // FYI this prevent default submit functionality because we're using KO.js
             console.log('TEST: Look in console');
-            console.log('Search: ' + searchTerm)
+            console.log('Search: ' + searchTerm);
             console.log('zipCode: ' + zipCode)
         }
     }
+
     // The's landing page code
     $(document).ready(function () {
-        ko.applyBindings(new LandingViewModel());
+        ko.applyBindings(new LandingViewModel(), document.getElementById("form-content"));
     });
 
 });
