36f5a1a6ed1c113a9469f1a7bc9ffe79175ba58c
Yewbdar <Yewbdar.grima@gmail.com>
2018-03-21 11:00:37 -0500

    update the json structure

--- assets/javascript/script/tests.js
+++ assets/javascript/script/tests.js
@@ -19,19 +19,44 @@ function testApiCall(options, apitoken){
 
 function getDbStructure() {
     return {
-        "event" : {
-            "date" : "today(march 18)",
-            "eventKey" : "2nub33ne7b4b4734bbeus(unique id) ",
-            "eventName" : "eating pizza ",
-            "location" : "downtown",
-            "time" : "10:00" //optional
-        },
-        "user" : {
-            "email" : "tom@gmail.com",
-            "eventKey" : "2nub33ne7b4b4734bbeus", //It's same with eventkey; this will enable us to easily get the group which a user belongs to.
-            "name " : "Tom",
-            "time" : "10:00",
-            "zipcode" : 77002
+        "events" : {
+          "mexicos-deli-houston" : {
+            "categories" : "chicken",
+            "eventName" : "Mexicos Deli",
+            "location" : {
+              "addess1" : "2374 S Dairy Ashford Rd",
+              "city" : "houston",
+              "zipCode" : 77077
+            },
+            "users" : [ {
+              "email" : "tom@gmail.com",
+              "firstName" : "Tom",
+              "lastName" : "David"
+            }, {
+              "email" : "me@you.com",
+              "fistName" : "me",
+              "lastName" : "you"
+            } ]
+          },
+          "ramen-bar-ichi-houston" : {
+            "categories" : "chicken",
+            "eventName" : "Ramen Bar Ichi",
+            "location" : {
+              "address1" : "2374 S Dairy Ashford Rd",
+              "city" : "houston",
+              "zipcode" : "77077"
+            },
+            "user" : [ {
+              "email" : "Ali@gmail.com",
+              "firstName" : "Ali",
+              "lastName" : "Ali"
+            }, {
+              "email" : "letseat@yahoo.com",
+              "firstName" : "lets",
+              "lastName" : "eat"
+            } ]
+          }
         }
-    };
+      };
+      
 }
