daa52ea058b4f6d08b6aefe035141cc1d24706eb
thaiscmky <22459337+thaiscmky@users.noreply.github.com>
2018-03-22 20:00:37 -0500

    Revert "Merge pull request #70 from thaiscmky/display-results"; needs to be fixed on staging first
    This reverts commit e7238442b7e7edaad65382749a707c3ac8cf5d31, reversing
    changes made to c8094bad72f32a10cdf4562ac0c1b2d1a04270bd.

--- assets/javascript/script/main.js
+++ assets/javascript/script/main.js
@@ -1,189 +1,18 @@
-define(["jquery", "bootstrap", "corsanywhere", "ko", "koDebug"], function ($, bootstrap, cors, ko, kob) {
-    function LetsEatViewModel() {
+define(["jquery", "bootstrap", "corsanywhere", "ko", "koDebug"], function($, bootstrap, cors, ko, kob) {
+    function LandingViewModel() {
         var self = this;
 
-        // Landing Observables
         self.searchTerm = ko.observable('');
+
         self.zipCode = ko.observable('');
+
         self.zipInfo = ko.observable('');
-
-
-        // These observable keep track of what page is displaying
-        self.landingVisible = ko.observable(true);
-        self.resultsVisible = ko.observable(false);
-        self.createVisible = ko.observable(false);
-        self.userVisible = ko.observable(false);
-
-        //this an array of arrays representing our bootstrap grid. Each item is an array of 3 objects representing
-        // a row. When we receive the events array from our database we will need to configure it this way using a function.
-        self.results = ko.observableArray([[{
-            restaurant: "Killen's BBQ",
-            image: "./assets/Killens-Line.jpg",
-            people: [{ name: 'Bubba' }, { name: "Yellow" }, { name: "Johnny" }],
-            location: {
-                city: "San Francisco",
-                country: "US",
-                address2: "",
-                address3: "",
-                state: "CA",
-                address1: "375 Valencia St",
-                zip_code: "94103"
-            },
-            categories: [
-                {
-                    alias: "coffee",
-                    title: "Coffee & Tea"
-                }
-            ],
-            rating: 4,
-            price: "$",
-            phone: "+14152520800"
-
-        },
-        {
-            restaurant: "Killen's BBQ",
-            image: "./assets/Killens-Line.jpg",
-            people: [{ name: 'Bubba' }],
-            location: {
-                city: "San Francisco",
-                country: "US",
-                address2: "",
-                address3: "",
-                state: "CA",
-                address1: "375 Valencia St",
-                zip_code: "94103"
-            },
-            categories: [
-                {
-                    alias: "coffee",
-                    title: "Coffee & Tea"
-                }
-            ],
-            rating: 4,
-            price: "$",
-            phone: "+14152520800"
-        },
-        {
-            restaurant: "Killen's BBQ",
-            image: "./assets/Killens-Line.jpg",
-            people: [{ name: 'Bubba' }],
-            location: {
-                city: "San Francisco",
-                country: "US",
-                address2: "",
-                address3: "",
-                state: "CA",
-                address1: "375 Valencia St",
-                zip_code: "94103"
-            },
-            categories: [
-                {
-                    alias: "coffee",
-                    title: "Coffee & Tea"
-                }
-            ],
-            rating: 4,
-            price: "$",
-            phone: "+14152520800"
-        }], [{
-            restaurant: "Killen's BBQ",
-            image: "./assets/Killens-Line.jpg",
-            people: [{ name: 'Bubba' }],
-            location: {
-                city: "San Francisco",
-                country: "US",
-                address2: "",
-                address3: "",
-                state: "CA",
-                address1: "375 Valencia St",
-                zip_code: "94103"
-            },
-            categories: [
-                {
-                    alias: "coffee",
-                    title: "Coffee & Tea"
-                }
-            ],
-            rating: 4,
-            price: "$",
-            phone: "+14152520800"
-        },
-        {
-            restaurant: "Killen's BBQ",
-            image: "./assets/Killens-Line.jpg",
-            people: [{ name: 'Bubba' }],
-            location: {
-                city: "San Francisco",
-                country: "US",
-                address2: "",
-                address3: "",
-                state: "CA",
-                address1: "375 Valencia St",
-                zip_code: "94103"
-            },
-            categories: [
-                {
-                    alias: "coffee",
-                    title: "Coffee & Tea"
-                }
-            ],
-            rating: 4,
-            price: "$",
-            phone: "+14152520800"
-        },
-        {
-            restaurant: "Killen's BBQ",
-            image: "./assets/Killens-Line.jpg",
-            people: [{ name: 'Bubba' }],
-            location: {
-                city: "San Francisco",
-                country: "US",
-                address2: "",
-                address3: "",
-                state: "CA",
-                address1: "375 Valencia St",
-                zip_code: "94103"
-            },
-            categories: [
-                {
-                    alias: "coffee",
-                    title: "Coffee & Tea"
-                }
-            ],
-            rating: 4,
-            price: "$",
-            phone: "+14152520800"
-        }], [{
-            restaurant: "Killen's BBQ",
-            image: "./assets/Killens-Line.jpg",
-            people: [{ name: 'Bubba' }],
-            location: {
-                city: "San Francisco",
-                country: "US",
-                address2: "",
-                address3: "",
-                state: "CA",
-                address1: "375 Valencia St",
-                zip_code: "94103"
-            },
-            categories: [
-                {
-                    alias: "coffee",
-                    title: "Coffee & Tea"
-                }
-            ],
-            rating: 4,
-            price: "$",
-            phone: "+14152520800"
-        }]
-        ]);
-
 
         // Sends AJAX to google APIs and sets VM's zipInfo to relevant JSON data.
         self.zipRequest = ko.computed(function () {
             // TODO: VALIDATOR: Below ajax call should only run when "self.zipInfo() === valid Zip Code
             // ELSE it should set self.zipInfo to something like "Zip Code not recognized"
-            if (self.zipCode() !== null || typeof self.zipCode() !== 'undefined') {
+            if(self.zipCode() !== null || typeof self.zipCode() !== 'undefined'){
                 $.ajax({
                     url: "http://maps.googleapis.com/maps/api/geocode/json?address=" + self.zipCode(),
                     method: "GET"
@@ -198,45 +27,17 @@ define(["jquery", "bootstrap", "corsanywhere", "ko", "koDebug"], function ($, bo
             var searchTerm = self.searchTerm();
             var zipCode = self.zipCode();
             // When user clicks submit this is the code that runs...
-          
-            // FYI this prevent default submit functionality because we're using KO.js                
-
-            // Hi this is needs to eventually automatically detect if event are found near you or not
-            if (confirm('Found events near you?\nOk = Navigates to Results Display\nCancel = Navigates to Create Display')) {
-                // Navigates to results display
-                self.landingVisible(false);
-                self.resultsVisible(true);
-            } else {
-                // Navigates to create display
-                self.landingVisible(false);
-                self.createVisible(true);
-            }
-        }
-
-        self.joinEvent = function () {
-            console.log("Joined Event");
-            self.resultsVisible(false);
-            self.userVisible(true);
-        }
-
-        self.submitUserInfo = function () {
-            console.log("Submitted user Info");
-            self.userVisible(false);
-            self.resultsVisible(true);
-        }
-
-        self.navToCreate = function () {
-            self.resultsVisible(false);
-            self.createVisible(true);
-        }
-        
-        self.navToResult = function () {
-            self.createVisible(false);
-            self.resultsVisible(true);
+            // FYI this prevent default submit functionality because we're using KO.js
+            console.log('TEST: Look in console');
+            console.log('Search: ' + searchTerm);
+            console.log('zipCode: ' + zipCode)
         }
     }
 
-    ko.applyBindings(new LetsEatViewModel);
+    // The's landing page code
+    $(document).ready(function () {
+        ko.applyBindings(new LandingViewModel(), document.getElementById("form-content"));
+    });
 
 });
 
